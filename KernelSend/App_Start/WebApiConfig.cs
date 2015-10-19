b60ae5ae83f777cbf13d32479bfdffab73a272d2
Angel G. <garaydev@gmail.com>
2015-10-18 23:30:53 -0400

    Added ProductController and methods.

--- KernelSend/App_Start/WebApiConfig.cs
+++ KernelSend/App_Start/WebApiConfig.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using KernelSend.Models;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
 
 namespace KernelSend
 {
@@ -9,16 +12,12 @@ namespace KernelSend
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
         }
     }
 }
---
+++ KernelSend/Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+using KernelSend.Models;
+
+namespace KernelSend.Controllers
+{
+    public class ProductsController : ODataController
+    {
+
+        AdventureWorks2008R2Entities db = new AdventureWorks2008R2Entities();
+
+        //test to see if any product exists
+        private bool ProductExists(int key)
+        {
+            return db.Products.Any(p => p.ProductID == key);
+        }
+
+        /// <summary>
+        /// Dispose the dispose
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        //Get All Products
+        [EnableQuery]
+        public IQueryable<Product> Get()
+        {
+            return db.Products;
+        }
+
+        //SingleResult of Product based on int parameter
+        [EnableQuery]
+        public SingleResult<Product> Get([FromODataUri] int key)
+        {
+            IQueryable<Product> result = db.Products.Where(p => p.ProductID == key);
+            return SingleResult.Create(result);
+        }
+
+        /// <summary>
+        /// Async Task action to create a new Product
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+
+        public async Task<IHttpActionResult> Post(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Products.Add(product);
+            await db.SaveChangesAsync();
+            return Created(product);
+        }
+    }
+}
